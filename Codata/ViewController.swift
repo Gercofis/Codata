5c7359e4643c6b116c81dc8d665eaae1fab474fa
gilles Robert <gillesrobert@free.fr>
2018-08-11 23:48:18 +0200

    Initial Commit

---
+++ Codata/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Codata
+//
+//  Created by Gilles ROBERT on 11/08/2018.
+//  Copyright © 2018 Gilles ROBERT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
